f7dd4b97cee759a5909158f746974d529194c114
Rafael Zampieron <rafaelprimieri@hotmail.com>
2023-11-16 16:49:36 -0300

    Update script.js

--- 04-aluraFlix/script.js
+++ 04-aluraFlix/script.js
@@ -1,6 +1,38 @@
-function adicionarFilme() {
-  var filmeFavorito = document.getElementById('filme').value
-  var elementoListaFilmes = document.getElementById('listaFilmes')
-  elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + '<img src=' + filmeFavorito + '>'
-  document.getElementById('filme').value = ''
+var listaFilmesFavoritos = [];
+var listaTrailersFilmesFavoritos = [];
+
+function adicionarFilme(){
+  var filmeFavorito = document.getElementById('filme').value;
+  var trailerFilmeFavorito = document.getElementById('trailer').value;
+
+      if(( filmeFavorito.endsWith('jpg') ) || ( filmeFavorito.endsWith('jpeg') )){
+      
+      document.getElementById('mensagemDeErro').innerHTML = '';
+      listaFilmesFavoritos.push(filmeFavorito);
+      listaTrailersFilmesFavoritos.push(trailerFilmeFavorito);
+      
+      limpaCampos();
+      recarregarFilmes();
+      
+    } else {
+      
+      document.getElementById('mensagemDeErro').innerHTML = 'Endereço de imagem inválido, tente novamente';
+      limpaCampos();
+      
+    }
+  }
+
+function recarregarFilmes(){
+ 
+  var elementoListaFilmes = document.getElementById('listaFilmes');
+  elementoListaFilmes.innerHTML = '';
+  for(i=0; i < listaFilmesFavoritos.length ; i++){
+    elementoListaFilmes.innerHTML += `<a href=" ${listaTrailersFilmesFavoritos[i]} "><img src=" ${listaFilmesFavoritos[i]} "></a>`;
+    
+  }
+}
+
+function limpaCampos(){
+   document.getElementById('filme').value = '';
+   document.getElementById('trailer').value = '';
 }
